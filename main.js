563bb7b22cbe94f26e1df2737fc5d95f994ebe1f
jspark9703 <67131959+jspark9703@users.noreply.github.com>
2020-10-09 17:11:13 +0900

    navbar menu click

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// navbar when it is on the top
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -10,3 +10,15 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+
+//scroll to section
+const contectMeBtn = document.querySelector(".home__button");
+contectMeBtn.addEventListener("click", scrollsection);
+navbar.addEventListener("click", scrollsection);
+function scrollsection(event) {
+  const target = event.target;
+  const tosection = target.dataset.to;
+  document
+    .querySelector(`#${tosection}`)
+    .scrollIntoView({ behavior: "smooth" });
+}
